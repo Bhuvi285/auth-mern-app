a8a4e29ee6b5ca66dfe71f2493864b473e2519ef
Bhuvanesh <bhuvineve05@gmail.com>
2026-03-15 12:49:24 +0530

    add ProductRouter

--- Backend/index.js
+++ Backend/index.js
@@ -3,20 +3,20 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const AuthRouter = require('./Routes/AuthRouter');
+const ProductsRouter = require('./Routes/ProductsRouter');
+
 
 require('dotenv').config(); // Load environment variables from .env file
 require('./Models/db')
 const PORT = process.env.PORT || 8080;
 
-app.get('/ping' , (req , res)=>{
-    res.send('POMG');
-})
-
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use('/auth' , AuthRouter );
+app.use('/auth', AuthRouter);
+//just for demonstration 
+app.use('/products', ProductsRouter);
 
-app.listen(PORT , ()=>{
+app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`);
 })
