408906abd38c0d9b7c834ffc0dda11cd16d25fd0
Bhuvanesh <bhuvineve05@gmail.com>
2026-03-18 23:20:46 +0530

    Initial Commit

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify';
+import { handleError, handleSuccess } from '../utils';
 
 function Signup() {
 
@@ -10,6 +11,7 @@ function Signup() {
         password: ''
     })
 
+    const navigate = useNavigate();
     const handleChange = (e) => {
         const { name, value } = e.target;
         console.log(name, value);
@@ -18,8 +20,40 @@ function Signup() {
         setSignupInfo(copySignupInfo);
     }
  
-    const handleSignup = (e) => {
+    const handleSignup = async (e) => {
         e.preventDefault();
+        const { name , email , password} = signupInfo;
+        if( !name || !email || !password){
+            return handleError('All fields are required!!');
+        }
+        try {
+            const url = "http://localhost:8181/auth/signup";
+            const response = await fetch(url ,{
+                method:"POST" , 
+                headers: {
+                    'Content-Type' : 'application/json'
+                },
+                body: JSON.stringify(signupInfo)
+            } );
+
+            const result = await response.json();
+            const {success , message , error} = result;
+
+            if(success){
+                handleSuccess(message);
+                setTimeout(()=>{
+                    navigate('/login')
+                } , 1000)
+            }else if(error){
+                const details = error?.details[0].message;
+                handleError(details);
+            }else if(!success){
+                handleError(message);
+            }
+            console.log(result);
+        } catch (err) {
+            handleError(err);
+        }
     }
     return (
         <div className='container'>
