e0d71e46eac40ee685fb5adb2c290d23253b816c
Bhuvanesh <bhuvineve05@gmail.com>
2026-03-19 13:06:40 +0530

    enhance Home component with user greeting, logout functionality, and product fetching

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,8 +1,61 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import { handleError, handleSuccess } from '../utils';
+import { ToastContainer } from 'react-toastify';
 
 function Home() {
+  const [loggedInUSer, setLoggedInUser] = useState('');
+  const [products, setProducts] = useState('');
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setLoggedInUser(localStorage.getItem('loggedInUSer'))
+  }, [])
+
+  const handleLogout = (e) => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('loggedInUser');
+    handleSuccess('User Logged Out')
+    setTimeout(() => {
+      navigate('/login');
+    }, 1000)
+  }
+
+  const fetchProducts = async () => {
+    try {
+      const url = "http://localhost:8181/products";
+      const headers = {
+        headers: {
+          'Authorization': localStorage.getItem('token')
+        }
+      }
+      const response = await fetch(url, headers);
+      const result = await response.json();
+      console.log(result);
+      setProducts(result);
+    } catch (err) {
+      handleError(err);
+    }
+  }
+
+  useEffect(() => {
+    fetchProducts()
+  }, []);
   return (
-    <div>Home</div>
+    <div>
+      <h1>Welcome {loggedInUSer}!</h1>
+      <button onClick={handleLogout}>Logout</button>
+      <div>
+        {
+          products && products?.map ((item, index) => {
+            <ul key={index}>
+              <span>{item.name}: {item.price}</span>
+            </ul>
+          })
+        }
+      </div>
+      <ToastContainer />
+    </div>
   )
 }
 
