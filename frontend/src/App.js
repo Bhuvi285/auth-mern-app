68f151d02b17384c6c691b09605f75da7ae90986
Bhuvanesh <bhuvineve05@gmail.com>
2026-03-19 13:06:04 +0530

    implement authentication logic with private route for home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,22 @@ import './App.css';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
 import Home from './pages/Home';
+import { useState } from 'react';
+import RefreshHandler from './RefreshHandler';
 
 function App() {
+  const [isAuthenticated , setIsAuthenticated] = useState(false);
+  const PrivateRoute = ({element}) =>{
+      return isAuthenticated ? element : <Navigate to="/login" />
+  }
   return (
     <div className="App">
+      <RefreshHandler setIsAuthenticated={setIsAuthenticated}/>
       <Routes>
         <Route path='/' element={<Navigate to = "/login"/>} />
         <Route path='/login' element={<Login />} />
         <Route path='/signup' element={<Signup />} />
-        <Route path='/home' element={<Home />} />
+        <Route path='/home' element={<PrivateRoute element={<Home/>}/>} />
       </Routes>
     </div>
   );
